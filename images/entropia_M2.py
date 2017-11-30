a66c48523dbaf0ecb1bb3fc7aa3f4a13e357ca4c
Ivan Burbano <ivanmbur@gmail.com>
2017-11-29 22:58:02 -0500

    se termina el resumen y los agradecimientos

--- images/entropia_M2.py
+++ images/entropia_M2.py
@@ -1,11 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import rc
+
+rc("text", usetex = True)
 
 Lambda = np.linspace(0.001,0.999,1000)
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize = (7,5))
 ax.plot(Lambda, -(Lambda*np.log(Lambda) + (1-Lambda)*np.log(1-Lambda)), c = "k")
-ax.set_xlabel("$\lambda$")
-ax.set_ylabel("$S$")
+ax.set_xlabel("$\lambda$", fontsize = 15)
+ax.set_ylabel("$S$", fontsize = 15)
 fig.savefig("entropia_M2")
 plt.show()
